b9193c8940b44eeff517bc547496a10781c6ec85
Hasan Altaf <hasanaltaf2001@gmail.com>
2020-08-11 14:17:12 -0700

    responsive navbar

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,19 +1,58 @@
 import React from "react";
 import Navbar from "../NavBar";
 import Home from "../Home"
-import Container from "../Container"
+import {Container, MainContainer} from "../Container"
 import styles from "./layout.module.css";
 import Headroom from "react-headroom";
-import Footer from "../Footer"
+import emailIcon from "../images/email.png"
+import { GrLinkedin, GrGithub} from "react-icons/gr";
+import { MdEmail } from "react-icons/md";
+import { Flex, Box, Image } from "rebass"
 
-export default props => (
-  <div className={styles.layout}>
+
+export default function Layout(props){
+
+
+  return (
+    <div className={styles.layout}>
     <Headroom>
     <Navbar type={(props.home ? 'transparent' : 'solid')} />
     </Headroom>
     {props.home ? <Home /> : null}
-    <Container>{props.children}</Container>
+    {props.home ? <Container>{props.children}</Container> : <MainContainer>{props.children}</MainContainer>}
     <Footer />
   </div>
-);
+  )
+}
+
+const Footer = props => (
+  <footer className={styles.indexFooter}>
+  <Container>
+    <Flex className={styles.FlexBox}>
+      <Box mb={10} width={[1 / 2, 1 / 4, 1 / 5, 1 / 4]}>
+      <a className={styles.button} href="https://www.linkedin.com/in/hasanaltaf/" target="_blank">
+          <GrLinkedin />
+        </a>
+      </Box>
+      <Box mb={10} width={[1 / 2, 1 / 4, 1 / 5, 1 / 4]}>
+      <a className={styles.button} href="mailto:hasanaltaf2001@gmail.com" target="_blank">
+        <MdEmail />
+        </a>
+      </Box>
+      <Box mb={10} width={[1 / 2, 1 / 4, 1 / 5, 1 / 4]}>
+      <a className={styles.button} href="https://github.com/haltaf19" target="_blank">
+        <GrGithub />
+        </a>
+      </Box>
+    </Flex>
+
+    <Flex className={styles.FlexBox}>
+      <Box width={1}>
+        <em className={styles.content}>Made With Love By Hasan Altaf ©{new Date().getFullYear()}</em>
+      </Box>
+    </Flex>
+  </Container>
+</footer>
+)
+
 
