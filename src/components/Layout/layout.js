13715925748f8bdadc538c0d1efddcb3c73a4aa5
Hasan Altaf <hasanaltaf2001@gmail.com>
2020-08-11 14:41:26 -0700

    refactor layout and footer

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -8,6 +8,7 @@ import emailIcon from "../images/email.png"
 import { GrLinkedin, GrGithub} from "react-icons/gr";
 import { MdEmail } from "react-icons/md";
 import { Flex, Box, Image } from "rebass"
+import Footer from "../Footer"
 
 
 export default function Layout(props){
@@ -24,35 +25,3 @@ export default function Layout(props){
   </div>
   )
 }
-
-const Footer = props => (
-  <footer className={styles.indexFooter}>
-  <Container>
-    <Flex className={styles.FlexBox}>
-      <Box mb={10} width={[1 / 2, 1 / 4, 1 / 5, 1 / 4]}>
-      <a className={styles.button} href="https://www.linkedin.com/in/hasanaltaf/" target="_blank">
-          <GrLinkedin />
-        </a>
-      </Box>
-      <Box mb={10} width={[1 / 2, 1 / 4, 1 / 5, 1 / 4]}>
-      <a className={styles.button} href="mailto:hasanaltaf2001@gmail.com" target="_blank">
-        <MdEmail />
-        </a>
-      </Box>
-      <Box mb={10} width={[1 / 2, 1 / 4, 1 / 5, 1 / 4]}>
-      <a className={styles.button} href="https://github.com/haltaf19" target="_blank">
-        <GrGithub />
-        </a>
-      </Box>
-    </Flex>
-
-    <Flex className={styles.FlexBox}>
-      <Box width={1}>
-        <em className={styles.content}>Made With Love By Hasan Altaf ©{new Date().getFullYear()}</em>
-      </Box>
-    </Flex>
-  </Container>
-</footer>
-)
-
-
