e71d7e5e4c865c7eb0e418865a6a07f0acb0c960
Hasan Altaf <hasanaltaf2001@gmail.com>
2020-08-17 20:23:43 -0700

    casing fix

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from "../NavBar";
+import Home from "../Home"
+import About from "../About"
+import Experience from "../Experience"
+import Education from "../Education"
+import AllProjects from "../AllProjects";
+// import Footer from "../Footer"
+import styles from "./Layout.module.css";
+import Headroom from "react-headroom";
+
+
+function Layout(props){
+
+  return (
+    <>
+    <div className={styles.layout}>
+    <Headroom>
+    <Navbar/>
+    </Headroom>
+    {props.home ? <Home /> : null}
+    {props.projects ? <AllProjects /> : null}
+    {props.about ? <About /> : null}
+    {props.experience? <Experience /> : null}
+    {props.education ? <Education /> : null}
+    </div>
+
+    {/* <Footer /> */}
+    </>
+  )
+}
+
+export default Layout
